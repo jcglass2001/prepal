ce0254839f9d4a5bbe07b8318c2f7970e53d92f2
John G <jcglass2001@gmail.com>
2025-04-15 15:45:22 -0400

    feat: created file for centralized access to environment variables

---
+++ etl/config/settings.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.getenv("POSTGRES_USER")
+db_password = os.getenv("POSTGRES_PASSWORD")
+db_name = os.getenv("POSTGRES_DB")
+db_host = os.getenv("POSTGRES_HOST")
+db_port = os.getenv("POSTGRES_PORT", 5432)
+
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_SERVERS")
+SUBMISSION_TOPIC = os.getenv("KAFKA_SUBMISSION_TOPIC")
+
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
