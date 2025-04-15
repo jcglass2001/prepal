1cfa1c3c4062b4f59172d805daab2ed1d405e36e
John G <jcglass2001@gmail.com>
2025-04-15 16:42:48 -0400

    refactor: renamed parameters

--- etl/config/settings.py
+++ etl/config/settings.py
@@ -10,6 +10,7 @@ db_host = os.getenv("POSTGRES_HOST")
 db_port = os.getenv("POSTGRES_PORT", 5432)
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_SERVERS")
-SUBMISSION_TOPIC = os.getenv("KAFKA_SUBMISSION_TOPIC")
+MEDIA_SUBMISSION_TOPIC = os.getenv("KAFKA_TOPIC")
+CONSUMER_GROUP_ID = os.getenv("KAFKA_CONSUMER_GROUP")
 
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
