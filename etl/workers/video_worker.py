35b5581495ab5a8f19b456355804b202d191a0b7
John G <jcglass2001@gmail.com>
2025-04-28 15:41:30 -0400

    feat etl/workers: added initial video_worker

---
+++ etl/workers/video_worker.py
@@ -0,0 +1,37 @@
+from utils.kafka_consumer import create_kafka_consumer 
+from config.settings import SUBMISSION_TOPIC
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+GROUP_ID = 'VIDEO_WORKER'
+
+def main():
+    consumer = create_kafka_consumer(
+        topic=SUBMISSION_TOPIC,
+        group_id=GROUP_ID
+    )
+
+    logging.info(f"{GROUP_ID} is running listeneing on topic {consumer.subscription()}")
+
+    try:
+     for message in consumer:
+        payload = message.value
+        if payload.get('media_type') == 'website_url':
+            logging.info(f"Received submission: {payload['source']}")
+            try:
+                logging.info("Processing payload...")
+                # process_video_payload(payload)
+            except Exception as e:
+                logging.error(f"Failed to process payload: {e}")
+    except KeyboardInterrupt:
+        logging.warn(f"KeyboardInterrupt: {GROUP_ID} no longer listening on topic {consumer.subscription()}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+    finally:
+        consumer.close()
+
+
+if __name__ == "__main__":
+    main()
+    
