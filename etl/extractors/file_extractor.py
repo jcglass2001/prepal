3089be9c6659708362347b1106ad06c622c8d086
John G <jcglass2001@gmail.com>
2025-04-29 12:10:14 -0400

    feat etl/extractors/file_extractor: added strategy dependency injection in constructor. fix etl/extractors/file_extractor: corrected method return type

--- etl/extractors/file_extractor.py
+++ etl/extractors/file_extractor.py
@@ -1,20 +1,25 @@
 
 from abc import ABC, abstractmethod
+from typing import Any
+from strategies.processing_strategy import ProcessingStrategy
 
 
 class FileExtractor(ABC):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, strategy: ProcessingStrategy) -> None:
+        self.strategy = strategy
 
     @abstractmethod 
-    def fetch_file_content(self, filepath: str) -> any: 
+    def fetch_file_content(self, filepath: str) -> Any: 
         pass 
 
     def extract(self, payload: dict) -> dict: 
         filepath = payload['source']
         content = self.fetch_file_content(filepath)
-        return {} # implement way to process file content 
+        return self.strategy.process(content, payload['media_type']) # implement way to process file content 
 
 class VideoFileExtractor(FileExtractor):
-    def fetch_file_content(self, filepath: str) -> any:
+    
+    def __init__(self, strategy: ProcessingStrategy) -> None:
+        super().__init__(strategy)
+    def fetch_file_content(self, filepath: str) -> Any:
         return None # implement later
