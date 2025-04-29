65df8652dc3ed36f633e82e8de7e33dbcb2e26a1
John G <jcglass2001@gmail.com>
2025-04-29 12:11:44 -0400

    feat etl/extractors/url_extractor: added strategy dependency injection in constructor. fix etl/extractors/url_extractor: corrected method return type

--- etl/extractors/url_extractor.py
+++ etl/extractors/url_extractor.py
@@ -1,30 +1,34 @@
-from abc import ABC, abstractmethod 
+from abc import ABC, abstractmethod
+from typing import Any 
 import httpx
+from strategies.processing_strategy import ProcessingStrategy
 
 class URLExtractor(ABC):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, strategy: ProcessingStrategy) -> None:
+        self.strategy = strategy
 
     @abstractmethod 
-    def fetch_url_content(self, url:str) -> any:
+    def fetch_url_content(self, url:str) -> Any:
         pass 
 
     def extract(self, payload: dict) -> dict:
         url = payload['source']
         content = self.fetch_url_content(url)
-        return {} # implement way to process content 
+        return self.strategy.process(content, media_type=payload['media_type']) # implement way to process content 
 
 class WebsiteExtractor(URLExtractor):
-    def __init__(self) -> None:
-        super().__init__()
     
-    def fetch_url_content(self, url: str) -> any:
+    def __init__(self, strategy: ProcessingStrategy) -> None:
+        super().__init__(strategy)
+
+    def fetch_url_content(self, url: str) -> Any:
         html_content = httpx.get(url)
         return html_content.text # temporary implementation. may change
 
 class VideoExtractor(URLExtractor):
-    def __init__(self) -> None:
-        super().__init__()
+    
+    def __init__(self, strategy: ProcessingStrategy) -> None:
+        super().__init__(strategy)
 
-    def fetch_url_content(self, url: str) -> any:
+    def fetch_url_content(self, url: str) -> Any:
         return None # implement logic related to video data 
