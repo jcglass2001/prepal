d42389ff8620862d2a1f750cb7218fb44e439421
John G <jcglass2001@gmail.com>
2025-05-02 21:31:58 -0400

    refactor config/: created class to hold environment constants. reserved behavior constants with yaml and secret/infra constants with .env

--- etl/config/settings.py
+++ etl/config/settings.py
@@ -1,20 +1,23 @@
 from dotenv import load_dotenv
+from utils.loader import load_yaml_config, get_config_path
 import os
 
 load_dotenv()
 
-db_user = os.getenv("POSTGRES_USER")
-db_password = os.getenv("POSTGRES_PASSWORD")
-db_name = os.getenv("POSTGRES_DB")
-db_host = os.getenv("POSTGRES_HOST")
-db_port = os.getenv("POSTGRES_PORT", 5432)
+_yaml_config = load_yaml_config(get_config_path())
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_SERVERS")
-SUBMISSION_TOPIC = os.getenv("KAFKA_TOPIC", "null")
-CONSUMER_GROUP_ID = os.getenv("KAFKA_CONSUMER_GROUP", "null")
+class Settings:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    KAFKA_BROKER = os.getenv("KAFKA_BROKER")
+    LLM_HOST = os.getenv("LLM_HOST", "http://localhost:11434/api/chat")
+    CONSUMER_GROUP_ID = os.getenv("KAFKA_CONSUMER_GROUP", "null")
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    ACCEPTED_MEDIA_TYPES = _yaml_config.get("accepted_media_types", [])
+    KAFKA_TOPIC = _yaml_config["kafka"]["topic"]
+    LLM_PROVIDER = _yaml_config["llm"]["provider"]
+    LLM_MODEL = _yaml_config["llm"]["model"]
+    USE_LLM = _yaml_config["llm"]["enabled"]
+    LOG_LEVEL = _yaml_config.get("log_level", "INFO")
+    WORKER_NAME = _yaml_config.get("worker_name","default_worker")
 
-LLM_HOST = os.getenv("LLM_HOST", "http://localhost:11434/api/chat")
-LLM_MODEL = os.getenv("LLM_MODEL", "none")
-USE_LLM = os.getenv("USE_LLM", "true")
+settings = Settings()
