0a6a1242a9585c336ed7120695d7a92e5b311085
John G <jcglass2001@gmail.com>
2025-05-02 21:33:26 -0400

    feat config/: added yaml configuration files that determine the behavior of worker

--- etl/config/__init__.py
+++ etl/config/__init__.py
@@ -1,1 +1,17 @@
-print("Config initialized...")
+import logging 
+from .settings import settings
+
+log_level = getattr(logging, settings.LOG_LEVEL.upper())
+logging.basicConfig(
+    level=log_level,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+logging.debug(
+    "\nWorker Configuration",
+    f"ID: {settings.WORKER_NAME}",
+    f"Kafka Topic: {settings.KAFKA_TOPIC}",
+    f"Media Types: {settings.ACCEPTED_MEDIA_TYPES}",
+    f"LLM Flag: {settings.USE_LLM}",
+    f"DB URL: {settings.DATABASE_URL}"
+)
