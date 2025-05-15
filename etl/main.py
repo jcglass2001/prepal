a6e80a21939403affdf2825ef5fd5db02985e88b
John G <jcglass2001@gmail.com>
2025-05-15 09:52:23 -0400

    refactor: modified main.py to make use of WatcherManager and properly handle graceful exits

--- etl/main.py
+++ etl/main.py
@@ -1,12 +1,31 @@
 import logging
+import signal
+import sys
 from watcher.drive_watcher import DriveWatcher
+from watcher.manager import WatcherManager
+
+
+
 def main():
-    print("ETL entry-point")
+    manager = WatcherManager()
+
+    def shutdown_handler(signum, frame):
+        logging.info(f"Received shutdown signal ({signum}). Stopping watchers...")
+        manager.stop_all()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, shutdown_handler)
+    signal.signal(signal.SIGTERM, shutdown_handler)
+
+
+    manager.start_watcher(DriveWatcher)
+    # manager.start_watcher(...)
+    logging.info("All watchers running...")
     
-    drive_watcher = DriveWatcher()
-    drive_watcher.run()
+    signal.pause()
+             
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level="DEBUG")
+    logging.basicConfig(level=logging.INFO)
     main()
