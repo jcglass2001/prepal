ab00c8e0c0e64fc45e2c8b3cfe56912ef6461cf5
John G <jcglass2001@gmail.com>
2025-05-23 17:16:18 -0400

    feat (processor/media.py): Implemented initial batch download - Created helper method '_download_file()' to query Drive API for download of file given file_id - Incorporated thread pool execution in class method 'download_media()' to concurrently query API for batch download. Currently have default workers set to 2

--- etl/processor/media.py
+++ etl/processor/media.py
@@ -1,20 +1,63 @@
-import logging
+
+import os
 from pydrive2.drive import GoogleDrive
+from pydrive2.files import ApiRequestError
 from redis import Redis
 from config.settings import app_config 
 from utils.client import setup_drive_client
-
-logging.basicConfig(level=app_config.LOG_LEVEL)
+from utils.logging import setup_logger 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class MediaProcessor:
     def __init__(self, file_ids: list[str], drive_client: GoogleDrive, redis_client):
         self.file_ids = file_ids
         self.drive_client = drive_client
         self.redis_client = redis_client
-        self.logger = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
-    def download_media(self):
-        # queries drive client to download media by file_id
-        self.logger.debug(f"Downloading media...")
+        self.logger = setup_logger(self.__class__.__name__)
+    
+    def _download_file(self, file_id:str):
+        """
+        Queries API for file download based on file_id and returns path of downloaded file
+        """
+        TMP_DIR = "tmp/media"
+        os.makedirs(TMP_DIR, exist_ok=True)
+
+        file_path = os.path.join(TMP_DIR, f"{file_id}.mp4")
+
+        try:
+            self.logger.info(f"Attempting to download file: {file_id}")
+            file = self.drive_client.CreateFile({'id' : file_id})
+            file.GetContentFile(file_path)
+            self.logger.debug(f"Successfully downloaded file to: {file_path}")
+            return file_path
+        except ApiRequestError:
+            self.logger.error(f"Request error while downloading: {file_id}")
+        except Exception as e:
+            self.logger.error(f"Unhandled exception downloading: {file_id}: {e}")
+            
+    def download_media(self, max_workers=2):
+        """
+        Launches batch download using thread pool
+        """
+        self.logger.debug(f"Starting batch download for files: {self.file_ids}")
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_dict = {
+                executor.submit(self._download_file, file_id): file_id 
+                for file_id in self.file_ids
+            } # dict(Future, file_id)
+            
+            for future in as_completed(future_dict):
+                file_id = future_dict[future]
+                try:
+                    result = future.result()
+                    if result:
+                        self.logger.info(f"Successfully downloaded {file_id}")
+                        self.logger.debug(f"Result: {result}")
+                    else:
+                        self.logger.warning(f"Download failed for {file_id}")
+                except Exception as e:
+                    self.logger.error(f"Error in thread for {file_id}: {e}")
+            
         pass
 
     def process_media(self):
@@ -33,8 +76,12 @@ class MediaProcessor:
 drive_client = setup_drive_client()
 redis_client = Redis(host=app_config.REDIS_HOST, port=app_config.REDIS_PORT)
 
-
 def process_media_job(task_data: dict):
     file_ids = task_data['file_ids']
     processor = MediaProcessor(file_ids, drive_client, redis_client)
     processor.run()
+
+
+if __name__ == "__main__":
+    data = {"file_ids": ["1Xt1o0W0sF-z_MUtfXi-e9i3qVuQ9lLBt"]}
+    process_media_job(data)
